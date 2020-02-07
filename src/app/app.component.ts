8e9972bac163aefa681280451a74d6008c289b23
unknown <FinIQ@FinIQ678.FinIQ-SP.com>
2020-02-07 10:05:41 +0530

    t

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-start",
+  template: "<h1>Angular..</h1>"
 })
-export class AppComponent {
-  title = 'angularProject';
-}
+  
+export class AppComponent {}
